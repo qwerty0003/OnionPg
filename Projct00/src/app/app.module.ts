dc0b54d1f6a63f3a19aa5ebd378dfe940de03d1e
Andrea Rossi <vinrossiandrea96@gmail.com>
2023-06-08 19:13:20 +0200

    pretty nice
    titolo, animazione nuova, sezione aggiustata 1, sezione nuova 2

--- Projct00/src/app/app.module.ts
+++ Projct00/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageComponent
+    PageComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
