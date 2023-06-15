213038389a91a927d29c68b7805ecb17714d0bf7
Andrea Rossi <vinrossiandrea96@gmail.com>
2023-06-15 17:31:45 +0200

    end v1

--- Projct00/src/app/page/page.component.ts
+++ Projct00/src/app/page/page.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page.component.scss']
 })
 export class PageComponent implements OnInit {
+  loaded = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.loaded = true; // Dopo 3 secondi, impostato su true
+    }, 3000);
   }
 
 }
